7c19888da29bd14003bf2bcee73580d5015ef92c
Anton <didenkofficial@gmail.com>
2023-01-24 00:21:35 +0200

    hw - 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+start.addEventListener('click', startColor);
+stop.addEventListener('click', stopColor);
+
+function startColor() {
+  start.disabled = true;
+  stop.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColor() {
+  start.disabled = false;
+  stop.disabled = true;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
