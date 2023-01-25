c00a7cf6d0f04c7870ca9206515abcc1a4d06238
Anton <didenkofficial@gmail.com>
2023-01-25 20:03:56 +0200

    error correction

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,6 +11,7 @@ const minutesValue = document.querySelector('[data-minutes]');
 const secondsValue = document.querySelector('[data-seconds]');
 
 startButton.disabled = true;
+let dateValue = 0;
 
 const options = {
   enableTime: true,
@@ -18,54 +19,55 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    dateValue = selectedDates[0];
     if (selectedDates[0] <= new Date()) {
       Notify.failure('Please choose a date in the future');
     } else {
       startButton.disabled = false;
       startButton.addEventListener('click', countDownTime);
-
-      function countDownTime() {
-        const timer = setInterval(() => {
-          startButton.disabled = true;
-          const dataChoosen = selectedDates[0].getTime();
-          const nowTime = new Date().getTime();
-          const timeLeft = dataChoosen - nowTime;
-
-          const { days, hours, minutes, seconds } = convertMs(timeLeft);
-
-          daysValue.innerHTML = days < 10 ? addLeadingZero(days) : days;
-          hoursValue.innerHTML = hours < 10 ? addLeadingZero(hours) : hours;
-          minutesValue.innerHTML =
-            minutes < 10 ? addLeadingZero(minutes) : minutes;
-          secondsValue.innerHTML =
-            seconds < 10 ? addLeadingZero(seconds) : seconds;
-
-          if (timeLeft < 1000) {
-            clearInterval(timer);
-            startButton.disabled = false;
-          }
-        }, 1000);
-      }
-      function addLeadingZero(value) {
-        const stringValue = String(value);
-        return stringValue.padStart(2, '0');
-      }
-      function convertMs(ms) {
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-
-        const days = Math.floor(ms / day);
-        const hours = Math.floor((ms % day) / hour);
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-        return { days, hours, minutes, seconds };
-      }
     }
   },
 };
 
 flatpickr(input, options);
+
+function countDownTime() {
+  startButton.disabled = true;
+  input.disabled = true;
+  const timer = setInterval(() => {
+    const dataChoosen = dateValue.getTime();
+    const nowTime = new Date().getTime();
+    const timeLeft = dataChoosen - nowTime;
+
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+    daysValue.innerHTML = addLeadingZero(days);
+    hoursValue.innerHTML = addLeadingZero(hours);
+    minutesValue.innerHTML = addLeadingZero(minutes);
+    secondsValue.innerHTML = addLeadingZero(seconds);
+
+    if (timeLeft < 1000) {
+      clearInterval(timer);
+      startButton.disabled = false;
+    }
+  }, 1000);
+}
+
+function addLeadingZero(value) {
+  const stringValue = String(value);
+  return stringValue.padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
